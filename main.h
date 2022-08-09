07207b2118b65bdd494a0d0f67000e26f77775e0
nigat22keku <nigatuabebe8@gmail.com>
2022-08-09 06:34:39 -0700

    task pro

--- main.h
+++ main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
 int print_string(char *string);
 int _putchar(char c);
-
+int print_int(va_list i);
+int print_dec(va_list d);
 
 #endif /* MAIN_H*/
