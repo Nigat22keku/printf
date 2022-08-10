9168d9131f33989d63454fd701d57d79251f82b5
nigat22keku <nigatuabebe8@gmail.com>
2022-08-10 07:02:52 -0700

    hat

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "main.h" 
+#include <stdio.h> 
+
+/**
+*main - Entry point
+*Return: Always 0 
+*/
+int main(void) { int a; int b; char *str;
+
+	str = "school";
+	a = _printf("%r\n", "hello"); /*expected: notrebloh*/
+	printf("--->%d\n", a); /*expected: 10*/
+
+	b = _printf("%r\n", str); /*expected: loohcs*/
+	printf("%d\n", b); /*expected: 7*/
+
+	b = _printf("%r\n", str); /*expected: loohcs*/
+	printf("%d\n", b); /*expected: 7*/
+	return (0);
+}
